dbd8a2af2039df08b582cc2cdccad32b3dbf31a1
Umang Agarwal <63835975+umang345@users.noreply.github.com>
2023-11-10 11:05:29 +0530

    Update test_user_model.py correct test case

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -28,7 +28,7 @@ class UserModelTestCase(unittest.TestCase):
             u.password
 
     def test_password_verification(self):
-        u = User(password='fish')
+        u = User(password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
